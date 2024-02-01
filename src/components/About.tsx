4106a8fa2181aea4a6b85318e575fc94c1cb04d2
Jacek <limaniec@gmail.com>
2024-02-01 15:08:19 +0100

    add About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,17 @@ const About = () => {
   const { image, title, subtitle } = aboutData;
   return (
     <section className="section">
-      <div className="container mx-auto">jhh</div>
+      <div className="container mx-auto">
+        <div className="bg-accent-secondary/20 grid grid-col lg:grid-cols-2 rounded-3xl">
+          <div className="flex justify-center items-center">
+            <img src={image} alt="" />
+          </div>
+          <div className="flex flex-col justify-center gap-8 p-10">
+            <h3 className="h2">{title}</h3>
+            <p>{subtitle}</p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
